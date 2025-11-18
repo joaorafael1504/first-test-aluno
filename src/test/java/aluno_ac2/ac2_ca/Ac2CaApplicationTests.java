efe8c0c436439b608767fc17eef918cb512b8fb5
Milton Penha <miltonjmiltonj@gmail.com>
2025-11-17 23:22:38 -0300

    chore: att Tests AC2Application

--- src/test/java/aluno_ac2/ac2_ca/Ac2CaApplicationTests.java
+++ src/test/java/aluno_ac2/ac2_ca/Ac2CaApplicationTests.java
@@ -2,8 +2,9 @@ package aluno_ac2.ac2_ca;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.UseMainMethod;
 
-@SpringBootTest
+@SpringBootTest(useMainMethod = UseMainMethod.ALWAYS)
 class Ac2CaApplicationTests {
 
 	@Test
